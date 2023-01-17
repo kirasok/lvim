20e9de40aa5e14b80f61a10be5c60cae81673616
Kira Sokolova <kirillmokretsov@protonmail.ch>
2023-01-17 03:54:10 +0300

    Modify git blame

--- config.lua
+++ config.lua
@@ -260,14 +260,14 @@ lvim.plugins = {
     "sindrets/diffview.nvim",
     event = "BufRead",
   },
-  {
-    "f-person/git-blame.nvim",
-    event = "BufRead",
-    config = function()
-      vim.cmd "highlight default link gitblame SpecialComment"
-      vim.g.gitblame_enabled = 0
-    end,
-  },
+  -- {
+  --   "f-person/git-blame.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     vim.cmd "highlight default link gitblame SpecialComment"
+  --     vim.g.gitblame_enabled = 0 -- TODO: can't disable by default
+  --   end,
+  -- },
   {
     "romgrk/nvim-treesitter-context",
     config = function()
