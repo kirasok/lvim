9d83155a782541572bb8e91a1319c76fe09f141e
Kira Sokolova <kirillmokretsov@protonmail.ch>
2023-02-15 22:44:37 +0300

    Add markdown-preview

--- config.lua
+++ config.lua
@@ -414,6 +414,16 @@ lvim.plugins = {
           }
         end,
     },
+    {
+        "iamcco/markdown-preview.nvim",
+        build = "cd app && npm install",
+        init = function()
+          vim.g.mkdp_filetypes = {
+              "markdown"
+          }
+        end,
+        ft = { "markdown" },
+    },
 }
 
 -- highlight luasnip nodes
