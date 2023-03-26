b162158944a028f044a2453415ac9ed335450ca6
Kira Sokolova <Kyra256@proton.me>
2023-03-26 14:58:15 +0300

    feat(wilder): remove

--- config.lua
+++ config.lua
@@ -63,7 +63,7 @@ formatters.setup {
 }
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-  { name = "write_good" }, -- text/markdown/latex
+  -- { name = "write_good" }, -- text/markdown/latex
 }
 local code_actions = require "lvim.lsp.null-ls.code_actions"
 code_actions.setup {
@@ -248,6 +248,7 @@ lvim.plugins = {
   {
     -- autoclose and autorename html tags
     "windwp/nvim-ts-autotag",
+    ft = "html",
     config = function()
       require("nvim-ts-autotag").setup()
     end,
@@ -415,34 +416,6 @@ lvim.plugins = {
     config = function()
       vim.opt.conceallevel = 2
       vim.g["tex_conceal"] = "abdgm"
-    end,
-  },
-  {
-    -- nice autocomplete for :
-    'gelguy/wilder.nvim',
-    config = function()
-      local wilder = require('wilder')
-      wilder.setup({ modes = { ':', '/', '?' } })
-      wilder.set_option('pipeline', {
-        wilder.branch(
-          wilder.python_file_finder_pipeline({
-            -- to use ripgrep : {'rg', '--files'}
-            -- to use fd      : {'fd', '-tf'}
-            file_command = { 'find', '.', '-type', 'f', '-printf', '%P\n' },
-            -- to use fd      : {'fd', '-td'}
-            dir_command = { 'find', '.', '-type', 'd', '-printf', '%P\n' },
-            -- use {'cpsm_filter'} for performance, requires cpsm vim plugin
-            -- found at https://github.com/nixprime/cpsm
-            filters = { 'fuzzy_filter', 'difflib_sorter' },
-          }),
-          wilder.cmdline_pipeline(),
-          wilder.python_search_pipeline()
-        ),
-      })
-      wilder.set_option('renderer', wilder.popupmenu_renderer({
-        -- highlighter applies highlighting to the candidates
-        highlighter = wilder.basic_highlighter(),
-      }))
     end,
   },
   {
