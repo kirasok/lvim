202d52ed08ba3ea19998e594224b14db6e9ad815
Kira Sokolova <Kyra256@proton.me>
2023-03-29 15:48:35 +0300

    feat(snippets): add template for tex

---
+++ luasnippets/tex.lua
@@ -0,0 +1,74 @@
+---@diagnostic disable: undefined-global
+return {
+  s("ls", {
+    t({ "\\begin{itemize}", "\t\\item " }),
+    i(1),
+    d(2, rec_ls, {}), t({ "", "\\end{itemize}" }),
+    i(0),
+  }),
+  s("template", {
+    t({
+      "\\documentclass[a4paper]{IEEEtran}",
+      "",
+      "\\usepackage[utf8]{inputenc}",
+      "\\usepackage[T2A]{fontenc}",
+      "\\usepackage{textcomp}",
+      "\\usepackage{amsmath, amssymb, amsfonts, amsthm}",
+      "\\usepackage{import}",
+      "\\usepackage{pdfpages}",
+      "\\usepackage{transparent}",
+      "\\usepackage{xcolor}",
+      "",
+      "\\newcommand{\\incfig}[2][1]{%",
+      "\t\t\\def\\svgwidth{#1\\columnwidth}",
+      "\t\t\\import{./figures/}{#2.pdf_tex}",
+      "}",
+      "",
+      "\\newtheorem{theorem}{Theorem}[section]",
+      "\\newtheorem{lemma}[theorem]{Lemma}",
+      "",
+      "\\pdfsuppresswarningpagegroup=1",
+      "",
+      "\\hyphenation{op-tical net-works semi-conduc-tor IEEE-Xplore}",
+      "",
+      "\\title{",
+    }),
+    i(1),
+    t({
+      "}",
+      "\\author{",
+    }),
+    i(2),
+    t({
+      "}",
+      "",
+      "\\begin{document}",
+      "\\maketitle",
+      "\\markboth{",
+    }),
+    i(3, "Lectures|Seminars|Labs"),
+    t({
+      ", \\today}",
+      "{Shell \\MakeLowercase{\\textit{et al.}}: A Sample Article Using IEEEtran.cls for IEEE Journals}",
+      "",
+      "\\begin{abstract}",
+      "\t\t",
+    }),
+    i(4),
+    t({
+      "",
+      "\\end{abstract}",
+      "",
+      "\\tableofcontents",
+      "\\newpage",
+      "",
+      "",
+    }),
+    i(0),
+    t({
+      "",
+      "",
+      "\\end{document}"
+    })
+  })
+}
